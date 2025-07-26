e240f52a88ae125ee8c95ec4fe9f1cdb075c1906
Mathew Mbugua <mbuguam443@gmail.com>
2025-07-26 21:08:25 +0300

    add staticfiles

--- mynewproject/settings.py
+++ mynewproject/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 
 #............................................
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
